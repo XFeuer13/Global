b7c651237d26dd45ea1e655285d467b8dd768cad
XFeuer13 <xflap13@hotmail.com>
2023-09-04 21:44:53 -0600

    Archivo de conexion

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $pass = "12345678";
+    $db = "inventario";
+    $conexion = new mysqli($server,$user,$pass,$db);
+    
+    
+?>
--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
-        echo "Este es mi proyecto global";
-        
+        require("conexion.php");
+        echo "Este es mi proyecto global <br>";
+        echo "Estaria mejor en Derecho <br>";
+        echo " Yo soy la Documentacion <br>";
+        echo "No me pagan por subir links <br>";
+        echo "Esta bien no me paguen <br> <br>";
+
+        $query ="SELECT * FROM reporte";
+
+
+    if ($result = $conexion->query($query)) {
+
+		while ($row = $result->fetch_assoc()) {
+			$idRep = $row["idReporte"];
+               $idPro =$row["idProducto"];
+               $cantidad =$row["cantidad"];
+               $fecha =$row["fecha"];
+               $idTec =$row["idTecnico"];
+
+
+			echo $idRep ." " .  $idPro ." " .  $cantidad ." " .  $fecha . " " .  $idTec ."<br>";
+        }
+
+            /*freeresultset*/
+	$result->free();
+        }
 
 ?>
