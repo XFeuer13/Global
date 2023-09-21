2c5e74104b4a8246bf57bc6f4e65e08e69a50358
XFeuer13 <xflap13@hotmail.com>
2023-09-21 14:19:49 -0600

    Final

--- reporteP.php
+++ reporteP.php
@@ -2,7 +2,7 @@
 require('fpdf/fpdf.php');
 require("conexion.php");
 
-// Configura la zona horaria a México
+// Configura la zona horaria México
 date_default_timezone_set('America/Mexico_City');
 
 class PDF extends FPDF
@@ -31,27 +31,45 @@ class PDF extends FPDF
 
         $this->SetFont('Arial', '', 12);
         $this->Cell(0, 10, 'Generado por: Jose Manuel Rangel Cortes', 0, 1, 'C'); 
-        $this->Cell(0, 10, 'Tabla de Registros de Productos', 0, 1, 'C'); 
+        $this->Cell(0, 10, 'Tabla de Producto', 0, 1, 'C'); 
 
         // Agregar la fecha actual a la derecha
         $this->SetFont('Arial', '', 12);
         $this->SetX(-60); // Posiciona el cursor de escritura a la derecha
         $this->Cell(60, 10, 'Fecha: ' . date('d/m/Y'), 0, 1, 'C');
         $this->Ln(5);
-        $this->Cell(10, 10, "ID", 'LR', 0, 'C');
-        $this->Cell(80, 10, "Nombre del Producto", 'LR', 0, 'C');
-        $this->Cell(60, 10, "Número de Serie", 'LR', 0, 'C');
-        $this->Cell(40, 10, "Estado", 'LR', 1, 'C');
+
+
+        $posX = ($this->GetPageWidth() - 200) / 2;
+
+        $this->SetX($posX);
+        $this->Cell(10, 10, "ID", 'LRBT', 0, 'C');
+        $this->Cell(80, 10, "Nombre del Producto", 'LRBT', 0, 'C');
+        $this->Cell(60, 10, "Numero de Serie", 'LRBT', 0, 'C');
+        $this->Cell(40, 10, "Estado", 'LRBT', 1, 'C');
 
         $this->elementosAgregados = 0;
     }
 
     function Footer()
     {
-        // Puedes personalizar el pie de página si lo deseas
+
+        // Ruta relativa al código QR
+        $codigoQR = 'C:\AppServ\www\Global\Imagen\qr.png';
+
+        // Ajusta la posición del código QR en la esquina inferior derecha
+        $this->SetX(-50);
+        $this->Image($codigoQR, 260, 178, 30);
+
         $this->SetFont('Arial', '', 10);
         $this->SetY(-20);
-        $this->Cell(0, 10, 'Página ' . $this->PageNo(), 0, 0, 'L');
+        $this->Cell(0, 10, 'Pagina ' . $this->PageNo(), 0, 0, 'L'); // Cambia 'R' a 'L'
+
+        // Agrega la línea horizontal y el cambio de fuente en el Footer
+        $this->Line(10, 40, 290, 40); // Ajusta la línea horizontal
+        $this->SetFont('Arial', 'B', 12);
+
+        
     }
 
     function AgregarRegistros()
@@ -67,10 +85,13 @@ class PDF extends FPDF
             if ($this->elementosAgregados >= $this->elementosPorPagina) {
                 // Si se alcanza el límite de elementos por página, agrega una nueva página automáticamente
                 $this->AddPage();
-                $this->Header(); // Restablece la cabecera de la tabla
+                
             }
 
+            $posX = ($this->GetPageWidth() - 200) / 2; // 200 es el ancho total de las celdas
+
             // Configura los datos de cada registro
+            $this->SetX($posX); // Establece la posición X para centrar las celdas
             $this->Cell(10, 10, $row['idProducto'], 'LRB', 0, 'C');
             $this->Cell(80, 10, utf8_decode($row['nombre']), 'LRB', 0, 'C');
             $this->Cell(60, 10, utf8_decode($row['numeroSerie']), 'LRB', 0, 'C');
